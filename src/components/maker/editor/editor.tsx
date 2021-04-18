d8899f19a2c00f7da8fe09371d89dd0b2c70d181
ksy <ksy8230@hanmail.net>
2021-04-18 13:26:16 +0900

    feat: 애드폼

--- src/components/maker/editor/editor.tsx
+++ src/components/maker/editor/editor.tsx
@@ -3,18 +3,21 @@ import styles from './editor.module.css';
 import {CardType} from '../maker';
 import Card from '../card/card';
 import CardEditForm from '../cardEditForm/cardEditForm';
+import CardAddForm from '../card_add_form/card_add_form';
 
 type PropTypes = {
     cards : CardType[];
+    addCard: (card:CardType) => void;
 }
 
-const Editor = ({cards}: PropTypes) => {
+const Editor = ({cards, addCard}: PropTypes) => {
     return (
         <div className={styles.editor}>
             <h1>Editor</h1>
             {
                 cards.map(card => <CardEditForm card={card} />)
             }
+            <CardAddForm onAdd={addCard} />
         </div>
     );
 };
--- src/components/maker/maker.tsx
+++ src/components/maker/maker.tsx
@@ -71,11 +71,16 @@ const Maker = ({ authService }: PropTypes) => {
       }
     });
   });
+  
+  const addCard = (card: CardType) => {
+    const updated = [...cards, card];
+    setCards(updated);
+  };
   return (
     <section className={styles.maker}>
       <Header onLogout={onLogout} />
       <div className={styles.container}>
-        <Editor cards={cards} />
+        <Editor cards={cards}  addCard={addCard} />
         <Preview cards={cards} />
       </div>
       <Footer />
