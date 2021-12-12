e66fd9f4019c0efd5e0adc65b19c69bc85195e8d
ksy <ksy8230@hanmail.net>
2021-12-12 14:28:58 +0900

    feat: 이미지 로딩 처리

--- src/components/maker/image_file_input/image_file_input.tsx
+++ src/components/maker/image_file_input/image_file_input.tsx
@@ -1,17 +1,20 @@
-import React, {  SyntheticEvent, useRef } from 'react';
+import React, {  SyntheticEvent, useRef, useState } from 'react';
 import styles from './image_file_input.module.css';
 
 const ImageFileInput = ({imageUploader, name, onFileChange}:any) => { 
+    const [loading, setLoading] = useState(false);
     const inputRef = useRef<HTMLInputElement>(null);
     const onButtonClick = (event: SyntheticEvent) => {
         event.preventDefault();
         inputRef?.current?.click();
     };
     const onChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+        setLoading(true);
         const files = (event.target as HTMLInputElement).files;
         console.log(files?.[0])
         const uploaded = await imageUploader.upload(files?.[0]);
         console.log(uploaded)
+        setLoading(false);
         onFileChange({
             name: uploaded.original_filename, url: uploaded.url
         })
@@ -19,7 +22,8 @@ const ImageFileInput = ({imageUploader, name, onFileChange}:any) => {
     return (
         <div className={styles.container}>
             <input ref={inputRef} type="file" accept="image/*" name="file" onChange={onChange} />
-            <button className={styles.button} onClick={onButtonClick}>{name || 'No File'}</button>
+            {loading && <div>loading...</div>}
+            {!loading && <button className={styles.button} onClick={onButtonClick}>{name || 'No File'}</button>}
         </div>
     );
 };
